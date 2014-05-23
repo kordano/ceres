be8bbe06a0594ce4c60ad10a857d8253420ea12c
kordano <konrad.kuehne@rocketmail.com>
2014-05-23 10:55:35 +0200

    simplify tweet-related functions and outsource views

--- src/ceres/core.clj
+++ src/ceres/core.clj
@@ -1,87 +1,17 @@
 (ns ceres.core
   (:require [ring.util.response :as resp]
             [ring.middleware.params :refer [wrap-params]]
-            [net.cgrand.enlive-html :as enlive]
             [compojure.route :refer [resources]]
             [compojure.handler :refer [site]]
             [compojure.core :refer [GET POST defroutes]]
             [org.httpkit.server :refer [with-channel on-close on-receive run-server send!]]
-            [clojure.java.io :as io]
-            [ceres.warehouse :as warehouse]))
-
-(def news-accounts ["FAZ_NET" "dpa" "tagesschau" "SPIEGELONLINE" "SZ"])
-
-(enlive/deftemplate page
-  (io/resource "public/index.html")
-  []
-  [:body]
-  (enlive/append
-   (enlive/html
-    [:div.panel.panel-default
-     [:div.panel-heading
-      [:h3.panel-title "Ceres"]]
-     [:div.panel-body
-      [:div.table-responsive
-       [:table.table.table-striped
-        [:thead
-         [:tr
-          (map #(vec [:th %]) news-accounts)]]
-        [:tbody
-         [:tr
-          (map #(vec [:td %]) (warehouse/get-news-counts news-accounts))]]]]
-      [:form {:action "detail" :method "POST"}
-       [:select.form-control {:name "news-account"}
-        (map #(vec [:option {:value %} %]) news-accounts)]
-       [:input.btn.btn-primary {:type "submit"}]
-       ]]])))
-
-
-(enlive/deftemplate detail
-  (io/resource "public/index.html")
-  [{:keys [params]}]
-  [:body]
-  (enlive/append
-   (enlive/html
-    [:div.row
-     [:div.panel.panel-default.col-md-6
-      [:div.panel-heading
-       [:h3.panel-title (str "Tweets of @" (:news-account params))]]
-      [:div.panel-body
-       [:div.table-responsive
-        [:table.table.table-striped
-         [:tbody
-          (map
-           #(vec [:form {:method "POST" :action "/detail"}
-                  [:tr
-                   [:td (:text %)]
-                   [:input {:type "hidden"
-                            :value (:news-account params)
-                            :name "news-account"}]
-                   [:td
-                    [:input.btn.btn-default.btn-xs
-                     {:type "submit"
-                      :value (:id %)
-                      :name "tweet-id"}]]]])
-           (warehouse/get-news (:news-account params)))]]]]]
-     [:div.panel.panel-default.col-md-6
-      [:div.panel-heading
-       [:h3.panel-title "Related tweets of " (or (:tweet-id params) "")]]
-      [:div.panel-body
-       [:div.table-responsive
-        [:table.table.table-striped
-         [:tbody
-          (if (:tweet-id params)
-            (map
-             #(vec [:tr [:td %]])
-             (warehouse/find-related-tweets (:tweet-id params)))
-            "NuThin")]]]
-       ]]])))
+            [ceres.view :as view]))
 
 
 (defroutes all-routes
   (resources "/")
-  (POST "/detail" [] detail)
-  (GET "/*" [] (page)))
+  (POST "/detail" [] view/detail)
+  (GET "/*" [] (view/page)))
 
 
 #_(def server
--- src/ceres/warehouse.clj
+++ src/ceres/warehouse.clj
@@ -3,88 +3,121 @@
             [monger.collection :as mc]
             [monger.operators :refer :all]
             [monger.conversion :refer [from-db-object]]
+            [clj-time.format :as f]
             [clj-time.core :as t]))
 
+(def custom-formatter (f/formatter "E MMM dd HH:mm:ss Z YYYY"))
+
+(def db (mg/get-db (mg/connect) "athena"))
+
+(def coll "tweets")
+
+
+(defn minkowski-distance [x y r]
+  (Math/pow (reduce + (map #(Math/pow (Math/abs (- (first %) (second %))) r) (map vector x y))) (/ 1 r)))
+
+
+(def users {"Angelica" {"Blues Traveler" 3.5
+                        "Broken Bells" 2.0
+                        "Norah Jones" 4.5
+                        "Phoenix" 5.0
+                        "Slightly Stoopid" 1.5
+                        "The Strokes" 2.5
+                        "Vampire Weekend" 2.0}
+            "Bill" {"Blues Traveler" 2.0,
+                    "Broken Bells" 3.5
+                    "Deadmau5" 4.0
+                    "Phoenix" 2.0
+                    "Slightly Stoopid" 3.5
+                    "Vampire Weekend" 3.0}
+            "Chan" {"Blues Traveler" 5.0
+                    "Broken Bells" 1.0
+                    "Deadmau5" 1.0
+                    "Norah Jones" 3.0
+                    "Phoenix" 5
+                    "Slightly Stoopid" 1.0}
+            "Dan" {"Blues Traveler" 3.0
+                   "Broken Bells" 4.0
+                   "Deadmau5" 4.5
+                   "Phoenix" 3.0
+                   "Slightly Stoopid" 4.5
+                   "The Strokes" 4.0
+                   "Vampire Weekend" 2.0}
+            "Hailey" {"Broken Bells" 4.0
+                      "Deadmau5" 1.0
+                      "Norah Jones" 4.0
+                      "The Strokes" 4.0
+                      "Vampire Weekend" 1.0}
+            "Jordyn" {"Broken Bells" 4.5
+                      "Deadmau5" 4.0
+                      "Norah Jones" 5.0
+                      "Phoenix" 5.0
+                      "Slightly Stoopid" 4.5
+                      "The Strokes" 4.0
+                      "Vampire Weekend" 4.0}
+            "Sam" {"Blues Traveler" 5.0
+                   "Broken Bells" 2.0
+                   "Norah Jones" 3.0
+                   "Phoenix" 5.0
+                   "Slightly Stoopid" 4.0
+                   "The Strokes" 5.0}
+            "Veronica" {"Blues Traveler" 3.0
+                        "Norah Jones" 5.0
+                        "Phoenix" 4.0
+                        "Slightly Stoopid" 2.5
+                        "The Strokes" 3.0}})
+
+(let [user-1 (vals (users "Hailey"))]
+  (minkowski-distance [1 2 3] [2 3 4] 2))
 
 (defn inverted-index [news-accounts]
-  (let [conn (mg/connect)
-        db (mg/get-db conn "athena")
-        coll "tweets"
-        news-tweets (->> (mc/find db coll {"user.screen_name" {$in news-accounts}})
-                          seq
-                          (map #(from-db-object % true))
-                          (map #(vec [(:id %) %]))
-                          (into {}))]
-      (->> (mc/find db coll {"entities.user_mentions.screen_name" {$in news-accounts}})
-           seq
-           (map #(from-db-object % true))
-           (remove #(nil? (:retweeted_status %)))
-           (map #(vec [(-> % :retweeted_status :id) (:id %)]))
-           (remove #(nil? (first %))))))
+  (->> (mc/find db coll {"entities.user_mentions.screen_name" {$in news-accounts}})
+       seq
+       (map #(from-db-object % true))
+       (remove #(nil? (:retweeted_status %)))
+       (map #(vec [(-> % :retweeted_status :id) (:id %)]))
+       (remove #(nil? (first %)))))
 
 
 (defn find-related-tweets [tweet-id]
-  (let [conn (mg/connect)
-        db (mg/get-db conn "athena")
-        coll "tweets"]
-    (->> (mc/find db coll {"retweeted_status.id" (Long/parseLong tweet-id)})
-         seq
-         (map #(from-db-object % true)))))
+  (->> (mc/find db coll {"retweeted_status.id" (Long/parseLong tweet-id)})
+       seq
+       (map #(from-db-object % true))))
 
 
 (defn get-news [news-account]
-  (let [conn (mg/connect)
-        db (mg/get-db conn "athena")
-        coll "tweets"]
-    (->> (mc/find db coll {"user.screen_name" news-account})
-         seq
-         (map #(from-db-object % true))
-         )))
+  (->> (mc/find db coll {"user.screen_name" news-account})
+       seq
+       (map #(from-db-object % true))
+       (map #(update-in % [:created_at] (fn [x] (f/parse custom-formatter x))))))
 
 
-(defn get-news-tweets [news-accounts]
-  (let [conn (mg/connect)
-      db (mg/get-db conn "athena")
-        coll "tweets"]
-    (->> (mc/find db coll {"user.screen_name" {$in news-accounts}})
-         seq
-         (map #(from-db-object % true))
-         (map #(vec [(:id %) %]))
-         (into {}))))
+(defn get-mentions[news-account]
+  (->> (mc/find db coll {"entities.user_mentions.screen_name" news-account})
+       seq
+       (map #(from-db-object % true))))
 
 
-(defn get-news-counts [news-accounts]
- (let [conn (mg/connect)
-      db (mg/get-db conn "athena")
-      coll "tweets"]
-    (into {}
-          (map
-           #(->> (vec [% (-> (mc/find db coll {"user.screen_name" %})
-                             seq
-                             count)]))
-           news-accounts))) )
+(defn get-all-retweets [news-account]
+  (->> (mc/find db coll {"retweeted_status.user.screen_name" news-account})
+       seq
+       (map #(from-db-object % true))))
+
 
 (comment
 
   (def news-accounts ["FAZ_NET" "dpa" "tagesschau" "SPIEGELONLINE" "SZ"])
 
-  (->> (get-news-tweets news-accounts) ffirst)
+  (reduce + (map #(count (get-news %)) news-accounts))
 
-  (let [conn (mg/connect)
-        db (mg/get-db conn "athena")
-        coll "tweets"
-        id (->> (reduce (fn [old [k v]] (update-in old [k] #(conj (or %1 #{}) v))) {} (inverted-index news-accounts))
-                (map #(vec [(key %) (count (val %))]) )
-                vec
-                (sort-by second > )
-                (take 5 )
-                last
-                first)]
-    (-> (first (seq (mc/find db coll {"id" id})))
-        (from-db-object true)
-        :text))
+  (->> (get-news "dpa")
+       (sort-by :created_at t/after?))
 
+  (->> (reduce (fn [old [k v]] (update-in old [k] (fn [x] (conj (or x #{}) v)))) {} (inverted-index news-accounts)) (map #(vec [(key %) (count (val %))]))
+       (sort-by second >)
+       (map second)
+       frequencies
+       (sort-by val >)
+       time)
 
-
-
-  )
+)
