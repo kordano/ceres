78a8c656155c0b12c5995599e5bad96b465591ae
kordano <konrad.kuehne@rocketmail.com>
2014-05-29 19:05:26 +0200

    add logging

--- project.clj
+++ project.clj
@@ -8,18 +8,19 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.4"]
                  [clj-time "0.7.0"]
+
                  [compojure "1.1.8"]
                  [ring "1.2.2"]
                  [enlive "1.1.5"]
                  [http-kit "2.1.18"]
+
+                 [gezwitscher "0.1.1-SNAPSHOT"]
                  [com.novemberain/monger "2.0.0-rc1"]
-                 [org.clojure/data.json "0.2.4"]
                  [incanter "1.5.5"]
-                 [gezwitscher "0.1.0"]
-                 [com.ashafa/clutch "0.4.0-RC1"]
-                 [org.twitter4j/twitter4j-core "4.0.1"]
-                 [org.twitter4j/twitter4j-stream "4.0.1"]]
+
+                 [com.taoensso/timbre "3.2.1"]]
 
   :main ceres.core
 
--- src/ceres/core.clj
+++ src/ceres/core.clj
@@ -8,16 +8,19 @@
             [org.httpkit.server :refer [run-server]]
             [ceres.curator :refer [store]]
             [gezwitscher.core :refer [start-filter-stream]]
-            [ceres.assembler :refer [page detail]]))
+            [ceres.assembler :refer [page detail]]
+            [taoensso.timbre :as timbre]))
+
+(timbre/refer-timbre)
+(timbre/set-config! [:appenders :spit :enabled?] true)
+(timbre/set-config! [:shared-appender-config :spit-filename] "resources/collector.log")
 
 (def twitter-state
   {:credentials {:consumer-key (or (System/getenv "TWITTER_API_KEY") "****")
                  :consumer-secret (or (System/getenv "TWITTER_API_SECRET") "****")
                  :access-token (or (System/getenv "TWITTER_ACCESS_TOKEN") "****")
                  :access-token-secret (or (System/getenv "TWITTER_ACCESS_TOKEN_SECRET") "****")}
-   :handler (fn [status]
-              (do (store status)
-                  (println (str "[" (:created_at status) "] Storing " (:id status) " from " (:screen_name (:user status))))))
+   :handler (fn [status] (store status))
    :follow [114508061 18016521 5734902 40227292 2834511]
    :track ["@FAZ_NET" "@tagesschau" "@dpa" "@SZ" "@SPIEGELONLINE"]})
 
@@ -32,6 +35,9 @@
   (start-filter-stream twitter-state))
 
 (comment
+
+
+  (info "soso it's you")
 
   (def stop-stream (start-filter-stream twitter-state))
 
