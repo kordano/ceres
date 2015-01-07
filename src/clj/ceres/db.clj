f342a7c117761c5bed7e96d81b354b931639cbd2
kordano <konrad.kuehne@rocketmail.com>
2015-01-07 11:22:47 +0100

    add user and url to datomic

--- src/clj/ceres/db.clj
+++ src/clj/ceres/db.clj
@@ -47,6 +47,7 @@
 (defn init-schema [conn path]
   (transact-all conn (io/resource path)))
 
+
 (defn transact-user
   "doc-string"
   [conn {:keys [screen_name id created_at]}]
@@ -58,14 +59,63 @@
      :user/created (c/to-date created_at)}]))
 
 
+(defn find-user
+  "Query for user"
+  [db id]
+  (ffirst
+   (d/q '[:find ?r
+          :in $ ?id
+          :where
+          [?r :user/id ?id]]
+        db id)))
+
+
+(defn transact-url
+  "Transact url and meta data"
+  [conn {:keys [url author ts mongo-id]}]
+  (d/transact
+   conn
+   [{:db/id (d/tempid :db.part/user)
+     :url/author author
+     :url/address url
+     :url/ts ts
+     :url/initial-tweet mongo-id}]))
+
+
+(defn find-url
+  "Query for specific url-string"
+  [db url]
+  (d/q '[:find ?r
+         :in $ ?url
+         :where
+         [?r :url/address ?url]]
+       db url))
+
+
+(defn transact-publication
+  "Transact publication into datomic"
+  [conn {:keys [user mongo-id url ts]}]
+  (d/transact
+   conn
+   [{:db/id (d/tempid :db.part/user)
+     :publication/author user
+     :publication/tweet mongo-id
+     :publication/url url
+     :publication/ts ts}])
+  )
+
+
 (comment
 
   (def conn (scratch-conn))
 
+  (def ddb (d/db conn))
+
   (init-schema conn "schema.edn")
 
-  (doall
-   (pmap #(transact-user conn %) (mc/find-maps @db "users")))
+  (time
+   (doall
+    (pmap #(transact-user conn %) (mc/find-maps @db "users"))))
 
 
 
